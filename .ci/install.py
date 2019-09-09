5d35eab671f158454d8427e9d3db477540065382
Micheal Brockus <42789598+squidfarts@users.noreply.github.com>
2019-09-09 07:20:42 -0700

    Fixed pip Conan install with '1.19.0' -> '1.18.0'.

--- .ci/install.py
+++ .ci/install.py
@@ -15,7 +15,7 @@
 import subprocess
 import sys, os
 
-subprocess.check_call(["pip3", "install", "meson==0.50.0", "conan==1.19.0"])
+subprocess.check_call(["pip3", "install", "meson==0.50.0", "conan==1.18.0"])
 subprocess.check_call(["mkdir", ".ntmp"])
 subprocess.check_call(["curl", "-L", "https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip", "-o", ".ntmp/ninja-linux.zip"])
 subprocess.check_call(["unzip", ".ntmp/ninja-linux.zip", "-d", ".ntmp"])
